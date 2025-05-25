195878dcde3b5ec1901805fec938380f6be8fb23
Anurag Sharma <130685870+anurag-sharma17@users.noreply.github.com>
2025-05-26 00:32:40 +0530

    very bad day

--- frontend/notes-app/src/utils/axiosInstance.js
+++ frontend/notes-app/src/utils/axiosInstance.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { BASE_URL } from "./constants";
 
 const axiosInstance = axios.create({
-  baseURL: "https://notes-app-166l.onrender.com",
+  baseURL: BASE_URL,
   timeout: 10000,
   header: {
     "Content-Type": "application/json",
